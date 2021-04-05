ad232f998775c43d6db6a95f0cafe4258bacd5f5
JoosoebKim <outfox100@naver.com>
2021-04-06 06:36:23 +0900

    비밀정보 보호

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 const express = require('express');
 const app = express();
 const PORT = 5000
+const bodyParser = require('body-parser');
+const { User } = require('./models/User');
+
+const config = require('./config/key');
+
+app.use(bodyParser.urlencoded({extended: true})); // application/x-www-form-urlencoded
+app.use(bodyParser.json()); // application/json
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://admin:1234@cluster0.3sft5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
 app.get('/', (req, res) => res.send('Hello'));
+
+app.post('/register', (req, res) => {
+    // 회원 가입할 때 필요한 정보들을 client에서 가져오면 정보들을 데이터 베이스에 넣어준다.
+    const user = new User(req.body)
+    user.save((err, userInfo) => {
+        if(err) return res.json({success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}`));
